ebf6372bf22022acc1571040036cc0947e32e389
Luca Spycher <lucaspycher@students.berkeley.net>
2017-11-07 11:38:48 -0800

    more edits

--- hangman-script.js
+++ hangman-script.js
@@ -3,7 +3,7 @@
  */
 console.log("hello");
 
-var cars = ["ferrari", "maserati", "ford", "toyota"];
+var cars = ["ferrari", "maserati", "ford", "toyota", "honda", "lotus", "chevrolet"];
 var food = ["cheese", "tortellini", "ravioli", "pizza", "mascarpone", "biscuit", "muffin", "bread"];
 var animals = ["dog", "cat", "kangaroo", "lynx", "lion", "monkey", "squirrel", "pelican"];
 var randomWords = ["abstract", "random", "vinyl", "grave", "feather", "hose", "telescope", "science"];
@@ -17,25 +17,28 @@ function getWord(num) {
 }
 
 function startGame() {
-    makeBoard();
-
+    var board = document.getElementById("board");
+    board.innerHTML = "";
+    board.style.display = "inline-block";
+    document.getElementById("guessDiv").style.display = "block";
+    var word = getWord(document.getElementById("categorySelect").value);
+    for(var i = 0; i < word.length; i++) {
+        board.innerHTML += '<div class='+word[i]+'></div>';
+    }
 }
 
 function submitLetter() {
-    var letter = document.form.elements[0].value;
+    var input = document.getElementById('enterLetter').value;
     var categoryNum = document.getElementById("categorySelect").value;
+
     var word = getWord(categoryNum);
     for(var i = 0; i < word.length; i++) {
-        if(word[i] == letter) {
+        if(word[i] == input) {
 
         }
     }
 }
 
-function makeBoard() {
-    document.getElementById("keyboard").innerHTML = "";
-    var alphabet = "abcdefghijklmnopqrstuvwxyz";
-    for(var i = 0; i < alphabet.length; i++) {
-        document.getElementById("keyboard").innerHTML += "<div id="+alphabet[i]+">"+alphabet[i]+"</div>";
-    }
+function checkUserInput() {
+
 }
