95189ca3d2a9de55ad69d9c8d074edf5419534d0
Luca Spycher <lucaspycher@students.berkeley.net>
2017-11-09 11:40:01 -0800

    more edits

--- hangman-script.js
+++ hangman-script.js
@@ -4,35 +4,38 @@
 console.log("the ting goes popopop");
 
 var guessesLeft = 10;
+var word = "";
 var cars = ["ferrari", "maserati", "ford", "toyota", "honda", "lotus", "chevrolet"];
 var food = ["cheese", "tortellini", "ravioli", "pizza", "mascarpone", "biscuit", "muffin", "bread"];
-var animals = ["dog", "cat", "kangaroo", "lynx", "lion", "monkey", "squirrel", "pelican"];
-var randomWords = ["abstract", "random", "vinyl", "grave", "feather", "hose", "telescope", "science"];
+var animals = ["dog", "cat", "kangaroo", "lynx", "lion", "monkey", "squirrel", "pelican", "zebra"];
+var randomWords = ["abstract", "random", "vinyl", "grave", "feather", "hose", "telescope", "science", "hello"];
 
 var categories = [cars, food, animals, randomWords];
 
 function getWord(num) {
     var category = categories[num];
     var rand = Math.floor(Math.random() * category.length);
-    return category[rand];
+    word = category[rand];
 }
 
 function startGame() {
+    //getting word
+    getWord(document.getElementById("categorySelect").value);
+
     //setting up board
     var board = document.getElementById("board");
     board.innerHTML = "";
     board.style.display = "inline-block";
     document.getElementById("guessDiv").style.display = "block";
-    var word = getWord(document.getElementById("categorySelect").value);
     for(var i = 0; i < word.length; i++) {
         board.innerHTML += '<div class='+word[i]+'></div>';
     }
+
 }
 
 function checkEnter(event) {
     //var char = event.which || event.keyCode;
     var char = event.keyCode;
-    document.getElementById("demo").innerHTML = "Unicode CHARACTER code: " + char;
     if(char === 13) {
         submitLetter();
     }
@@ -41,8 +44,6 @@ function checkEnter(event) {
 function submitLetter() {
     console.log("letter submitted");
     var input = document.getElementById('enterLetter').value;
-    var categoryNum = document.getElementById("categorySelect").value;
-    var word = getWord(categoryNum);
     var inWord = false;
 
     for (var i = 0; i < word.length; i++) {
@@ -57,6 +58,8 @@ function submitLetter() {
         console.log("wrong guess");
         wrongGuess(input);
     }
+
+    document.getElementById("enterLetter").value = ''; //clears the input field
 }
 
 function wrongGuess(letter) {
